8713c88d87d6a8b08b4c68412f27a82c81c68f31
Myskyscraper <850194915@qq.com>
2018-01-16 16:49:24 +0800

    pro init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+   <div class="box" v-on:click="show"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    show(){
+      console.log(this.$route.path);
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.box{
+  width: 300px;
+  height: 300px;
+  border: 1px solid red;
+}
+</style>
